aa303d8f5c57d25ae2b8b975c22e747884ef4e99
Muskan3dev <muskannovember3@gmail.com>
2023-12-01 14:07:45 +0530

    Added fs.read() method

--- main.ts
+++ main.ts
@@ -50,3 +50,24 @@ if(err){
 console.log("Asynchronous read:"+data.toString());
  });
 });
+
+/*Reading a file using -fs.read(fd, buffer, offset, length, position, callback)-This
+ This method use file descriptor to read the file*/
+var buf=new Buffer(1024);
+console.log("Going to open existing file");
+fs.open('input.txt', 'r+' , function(err,fd){
+if(err){
+    console.error(err);
+}
+console.log("File opened successfully");
+console.log("Going to read the file");
+fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+if(err){
+    console.error(err);
+}
+console.log(bytes+"bytes read")
+if(bytes<0){
+    console.log(buf.slice(0,bytes).toString());
+}
+});
+});
