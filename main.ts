6f96812a2f2a07f2d345b7a098c02cf8b4427915
Muskan3dev <muskannovember3@gmail.com>
2023-12-01 15:04:43 +0530

    Added fs.ftruncate() method

--- main.ts
+++ main.ts
@@ -99,3 +99,38 @@ console.log("File closed successfully");
     });
 });
 });
+
+//Truncate a file-truncate-fs.ftruncate(fd, len, callback)
+console.log("Going to truncate a file");
+console.log("Going to open an existing file");
+fs.open('input.txt', 'r+' , function(err,fd){
+if(err){
+    console.error(err);
+}
+console.log("File opened successfully");
+
+fs.ftruncate(fd, 10, function(error){
+if(err){
+    console.error(err);
+}
+console.log("Going to read the same file"); 
+      
+      fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+         if (err) {
+            console.log(err);
+         }
+
+         // Print only read bytes to avoid junk.
+         if(bytes > 0) {
+            console.log(buf.slice(0, bytes).toString());
+         }
+         // Close the opened file.
+         fs.close(fd, function(err) {
+            if (err) {
+               console.log(err);
+            } 
+            console.log("File closed successfully.");
+        });
+    });
+});
+});
