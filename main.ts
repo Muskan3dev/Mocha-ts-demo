b6ae362688eeb9d17a2e27fc5e6cc7bf72cb004c
Muskan3dev <muskannovember3@gmail.com>
2023-12-01 12:35:18 +0530

    Added fs.stat() to get file information

--- main.ts
+++ main.ts
@@ -1,15 +1,36 @@
-var fs=require("fs");
+var fs = require("fs");
 // Asynchronous read
 fs.readFile('input.txt', function (err, data) {
     if (err) {
-       return console.error(err);
+        return console.error(err);
     }
     console.log("Asynchronous read: " + data.toString());
- });
- 
- // Synchronous read
- var data = fs.readFileSync('input.txt');
- console.log("Synchronous read: " + data.toString());
- 
- console.log("Program Ended");
- 
+});
+
+// Synchronous read
+var data = fs.readFileSync('input.txt');
+console.log("Synchronous read: " + data.toString());
+
+console.log("Program Ended");
+
+//Going to open file
+console.log("Going to open file");
+fs.open('input.txt', 'r+', function (err, fd) {
+    if (err) {
+        console.error(err);
+    }
+    console.log("File opened successfully");
+});
+
+//getting file information-using fs.stat(path,callback)
+console.log("Going to get file info");
+fs.stat("input.txt",function(err,stats) {
+    if(err){
+        console.error(err);
+    }
+    console.log(stats);
+    console.log("Got file info successfully");
+    //check file type
+    console.log("isFile? ", stats.isFile());
+    console.log("isDirectory? ", stats.isDirectory());
+});
